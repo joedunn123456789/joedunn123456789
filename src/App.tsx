2f0990e66d7896ab804859348864566c7f5d10cb
joedunn123456789 <129883064+joedunn123456789@users.noreply.github.com>
2025-10-07 00:44:51 -0700

    switch to hashrouter for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"  // ← Change this line
 import Shell from "./Shell"
 import Home from "./pages/Home"
 import Resume from "./pages/Resume"
@@ -8,14 +8,16 @@ import Analytics from "./pages/Analytics"
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Shell />}>
-        <Route index element={<Home />} />
-        <Route path="resume" element={<Resume />} />
-        <Route path="crm" element={<CRM />} />
-        <Route path="store" element={<Store />} />
-        <Route path="analytics" element={<Analytics />} />
-      </Route>
-    </Routes>
+    <Router>  {/* ← This now uses HashRouter */}
+      <Routes>
+        <Route path="/" element={<Shell />}>
+          <Route index element={<Home />} />
+          <Route path="resume" element={<Resume />} />
+          <Route path="crm" element={<CRM />} />
+          <Route path="store" element={<Store />} />
+          <Route path="analytics" element={<Analytics />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
