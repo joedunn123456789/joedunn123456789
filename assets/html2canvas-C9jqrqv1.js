4ed1e101c8949d04f7ddcd5764f436fb7af4f100
joedunn123456789 <129883064+joedunn123456789@users.noreply.github.com>
2025-10-16 23:07:53 -0700

    Updates

---
+++ assets/html2canvas-C9jqrqv1.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-kMdkHfoU.js";export default e();
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/joedunn123456789/', //match repo name
-  plugins: [react()],
-})
